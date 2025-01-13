09b663ca43887e975707604b6acdbd5a64042e08
naamalevi1357 <naamalevi1357@gmail.com>
2025-01-13 19:12:00 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,3 +1,36 @@
+-- 7
+DELETE from shopping WHERE name like 'Orange';
+--A query that deletes a row from a table,
+--It is always mandatory to specify the word WHERE after this 
+--Because we don't want every table to be deleted 
+--In the example, we set to delete the existing row in the NAME column named 'Orange'
+
+
+-- 8
+UPDATE shopping SET name = 'Bisli' WHERE name LIKE 'Bamba'
+UPDATE shopping SET amount=1 WHERE name LIKE 'Milk'
+
+--An UPDATE query is a query that updates existing values ​​in the table, the word SET is always present in the UPDATE query  
+--And it is always mandatory to specify the word WHERE after that because we do not want a value that we want to change to change the entire column in the table 
+--In example 1 we updated the NAME column to be equal to 'Bisli' where? In the position in the NAME column where the name 'Bamba' exists 
+--Example 2 We updated the amount column to be equal to amount 1 in the position in the NAME column whose name is 'Milk'
+
+-- 9
+
+ALTER TABLE shopping ADD COLUMN maavar
+
+-- An ALTER TABLE query is a query that adds the column to the existing table 
+--In this example we have ended a column called maavar
+  
+-- 10 
+UPDATE shopping SET maavar=6 WHERE id=1;
+UPDATE shopping SET maavar=3 WHERE id=2;
+UPDATE shopping SET maavar=12 WHERE id=3;
+UPDATE shopping SET maavar=8 WHERE id=4;
+UPDATE shopping SET maavar=5 WHERE id=5;
+--In this query we update the values ​​in the maavar 
+--column and then specify which row to specify (in this example we update the ID column)
+
 -- 11
 SELECT * FROM shopping WHERE amount > 1 AND maavar > 5
 SELECT * FROM shopping WHERE maavar BETWEEN 3 AND 5
